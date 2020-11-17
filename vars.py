68ecabd9cc303263d62094c72b187decce92f710
Аким Малыщик <akimich11@yandex.by>
2020-11-17 19:04:13 +0300

    убрал фичи с расписанием

--- vars.py
+++ vars.py
@@ -2,10 +2,8 @@ class User:
     def __init__(self, chat_id):
         self.username = "null"
         self.password = "null"
-        self.course = "null"
-        self.group = "null"
         self.chat_id = chat_id
-        self.subjects = []
+        self.subjects = times
         self.subjects_to_attend = []
 
 
@@ -14,11 +12,10 @@ class UserBooleans:
         self.chat_id = chat_id
         self.username = False
         self.password = False
-        self.course = False
-        self.group = False
         self.registered = False
 
 
 users = []
 user_booleans = []
 weekdays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+times = ["8:15", "9:45", "11:15", "13:00", "14:30", "Готово"]
