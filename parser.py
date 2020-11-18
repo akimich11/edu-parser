ddc028393177195361d333a2306860f14a77b6d1
Аким Малыщик <akimich11@yandex.by>
2020-11-18 17:17:58 +0300

    feat: Добавил обработку исключений

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium import webdriver
+from selenium import common
 
 
 # собственно вход
@@ -19,15 +20,46 @@ def enter_conference(driver, course_url):
     driver.find_element_by_xpath("//span[text()='Только слушать']").click()
 
 
+def try_to_click(element):
+    while True:
+        try:
+            element.click()
+            return
+        except common.exceptions.WebDriverException:
+            pass
+
+
+def edu_runner(driver, courses):
+    for i in range(len(courses)):
+        try_to_click(courses[i])
+        linked_image = driver.find_elements_by_xpath("//img[@src='https://edufpmi.bsu.by/theme/image.php/moove"
+                                                     "/attendance/1604991493/icon']")
+        for x in range(len(linked_image)):
+            attendance = linked_image[x]
+            attendance.click()
+            send_link = driver.find_elements_by_link_text("Отправить посещаемость")
+            if len(send_link) != 0:
+                send_link[-1].click()
+                driver.find_element_by_xpath("//span[text()='Присутствовал']").click()
+                driver.find_element_by_id("id_submitbutton").click()
+                # sleep(5)
+                driver.back()
+                driver.back()
+                # driver.back()
+            driver.back()
+            linked_image = driver.find_elements_by_xpath("//img[@src='https://edufpmi.bsu.by/theme/image.php/moove"
+                                                         "/attendance/1604991493/icon']")
+        driver.back()
+        courses = driver.find_elements_by_xpath("//span[@class='multiline']")
+
+
 if "__main__" == __name__:
     file = open("subjects_to_attend.txt")
     for line in file:
         data = line.replace("\n", "").split("|")
         usr, pwd = data[0].split(";")
-        subjects = data[1].split(";")
 
-        # попытка входа
-        dvr = webdriver.Chrome()
+        dvr = webdriver.Firefox()
         dvr.implicitly_wait(7)
 
         dvr.get("https://edufpmi.bsu.by")
@@ -35,26 +67,6 @@ if "__main__" == __name__:
         login(dvr, usr, pwd)
         if dvr.current_url == main_url:
             login(dvr, usr, pwd)
-        # получение списка курсов
-        courses = dvr.find_elements_by_xpath("//span[@class='multiline']")
 
-        for i in range(len(courses)):
-            courses[i].click()  # open course
-            linked_image = dvr.find_elements_by_xpath("//img[@src='https://edufpmi.bsu.by/theme/image.php/moove"
-                                                      "/attendance/1604991493/icon']")
-            for x in range(len(linked_image)):
-                attendance = linked_image[x]
-                attendance.click()
-                send_link = dvr.find_elements_by_link_text("Отправить посещаемость")
-                if len(send_link) != 0:
-                    send_link[-1].click()
-                    dvr.find_element_by_xpath("//span[text()='Присутствовал']").click()
-                    dvr.find_element_by_id("id_submitbutton").click()
-                    dvr.back()
-                    dvr.back()
-                    # dvr.back()
-                dvr.back()
-                linked_image = dvr.find_elements_by_xpath("//img[@src='https://edufpmi.bsu.by/theme/image.php/moove"
-                                                          "/attendance/1604991493/icon']")
-            dvr.back()
-            courses = dvr.find_elements_by_xpath("//span[@class='multiline']")
+        edu_runner(dvr, dvr.find_elements_by_xpath("//span[@class='multiline']"))
+        dvr.quit()
